5f8de20764899b9f95f5fda4fb5be867ed045211
Metin Balaban <balaban@tscc-login1.sdsc.edu>
2021-05-21 13:57:24 -0700

    rnasim variable size dataset

---
+++ singlegene/rnasim/variable-size/scripts/gen-replicates.sh
@@ -0,0 +1,56 @@
+#! /usr/bin/env bash
+
+#reproducible random number generator
+get_seeded_random()
+{
+  seed="$1"
+  openssl enc -aes-256-ctr -pass pass:"$seed" -nosalt \
+    </dev/zero 2>/dev/null
+}
+
+# create replicate directories
+mkdir -p data 
+
+for i in 500 1000 5000 10000 50000 100000 200000; do
+	for j in 0 1 2 3 4; do
+		mkdir -p data/$i/$j
+	done
+done
+
+mkdir -p tmp
+# compute novelty for each sequence
+
+bin/nw_distance -m p -s f -n  rnasim-source/true_topo.tree | sort -k2n > tmp/diversity.txt
+pushd tmp
+# this will create 20 quartiles with suffix 00 .. 19
+split --lines=10000 -d diversity.txt diversity.
+popd 
+
+for i in 0 1 2 3 4; do
+	for j in `seq -w 0 19`; do 
+		# sort each quartile randomly with seed = replicate number
+		# then select the top 10
+		sort --random-source=<(get_seeded_random $i) -R tmp/diversity.${j} | head -n 10
+	done | cut -f1 | sed "s/$/\t$j/g" > data/200000/$i/diversity_scores.txt
+	cut -f1 data/200000/$i/diversity_scores.txt > data/200000/$i/queries.txt
+	# create backboe and query alignment and backbone tree for 200000 subset
+	bin/faSomeRecords rnasim-source/aln_dna.fa data/200000/$i/queries.txt > data/200000/$i/query.fa
+	bin/faSomeRecords -exclude rnasim-source/aln_dna.fa data/200000/$i/queries.txt > data/200000/$i/ref.fa
+	cp rnasim-source/true_topo.tree data/200000/$i/true_topo.tree
+done 
+
+# use same queries accross replicates with same id
+for j in 0 1 2 3 4; do
+	
+	for i in 500 1000 5000 10000 50000 100000 ; do
+                cp data/200000/$j/queries.txt data/$i/$j
+		cp data/200000/$j/query.fa data/$i/$j
+        done
+done
+
+# create backboe and query alignment and backbone tree for 200000 subset
+
+
+
+
+
